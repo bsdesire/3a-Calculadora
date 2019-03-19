337a2e83d2693ad8a2ee28d456cc8bd44d7083c1
bsdesire <aluno19130@ipt.pt>
2019-03-19 17:02:48 +0000

    Introdução de números funcional

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -11,6 +11,44 @@ namespace Calculadora.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            //Inicializar o visor com o zero
+            ViewBag.Resposta = "0";
+            return View();
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(string visor, string btn)
+        {
+            string resposta = visor;
+            //avaliar o valor do btn
+            switch (btn)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    //determinar se o visor tem apenas o zero
+                    if (resposta == "0") //visor =="0"
+                    {
+                        resposta = btn;
+                    }
+                    else
+                    {
+                        resposta += btn;
+                    }
+                    break;
+            }
+
+
+            //devolver a 'resposta' para o visor do ecrã
+            ViewBag.Resposta = resposta;
             return View();
         }
     }
